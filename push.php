6f51e5d1ab3f831f38b1b8c3a2b381a354298c78
Leonardo Avila <lmavilaa@utn.edu.ec>
2023-07-19 03:33:46 -0500

    prueba 0.1.1

--- push.php
+++ push.php
@@ -1,12 +1,12 @@
 <?php
 
 $repoOwner = 'LeoAvila1911'; // Reemplaza con el nombre del propietario del repositorio
-$repoName = 'Prueba-integracion'; // Reemplaza con el nombre de tu repositorio
+$repoName = 'Integration-Zenodo-GitHub'; // Reemplaza con el nombre de tu repositorio
 $commitMessage = 'prueba 0.1.1'; // Reemplaza con el mensaje del commit
 $githubToken = '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'; // Reemplaza con tu token de acceso personal de GitHub
 
 $commitCommands = array(
-    'cd "C:\xampp\htdocs\Prueba api" && git add . && git commit -m "'.$commitMessage.'"',
+    'cd C:\xampp\htdocs\Prueba api && git add . && git commit -m "'.$commitMessage.'"',
     // Agrega más comandos para cada carpeta que desees incluir en el push
 );
 
